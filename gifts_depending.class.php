599d8d90e2bdf588058d245aeb578dbb0bdf2c05
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2017-08-26 00:34:48 -0500

    add styles

--- gifts_depending.class.php
+++ gifts_depending.class.php
@@ -5,9 +5,10 @@ class GiftsDependingPurchase {
         add_action( 'init', array($this,'gifts_depending_init') );
         add_action( 'add_meta_boxes', array($this, 'gifts_depending_meta_boxes') );
         add_action( 'save_post', array($this, 'gifts_depending_meta_save') );
-        add_filter('manage_gift_posts_columns', array($this, 'gifts_depending_columns_head') );
-        add_action('manage_gift_posts_custom_column', array($this, 'gifts_depending_columns_content'), 10, 2);
-        add_filter( 'manage_edit-gift_sortable_columns', array($this, 'my_sortable_gift_column') );
+        add_action( 'admin_enqueue_scripts', array($this, 'gifts_depending_admin_assets') );
+        add_filter( 'manage_gift_posts_columns', array($this, 'gifts_depending_columns_head') );
+        add_action( 'manage_gift_posts_custom_column', array($this, 'gifts_depending_columns_content'), 10, 2);
+        add_filter( 'manage_edit-gift_sortable_columns', array($this, 'gifts_depending_sortable_column') );
     }
 
     function gifts_depending_init() {
@@ -26,7 +27,7 @@ class GiftsDependingPurchase {
     // Registrar Meta box en los formularios
     function gifts_depending_meta_boxes() {
 
-        add_meta_box( 'gift-info', 'Información del Cupón', array($this, 'gifts_depending_info_content'), 'gift', 'normal', 'high' );
+        add_meta_box( 'gift-info', 'Information', array($this, 'gifts_depending_info_content'), 'gift', 'normal', 'high' );
 
     }
 
@@ -38,10 +39,10 @@ class GiftsDependingPurchase {
         wp_nonce_field( 'gifts_depending_meta_box_nonce', 'meta_box_nonce' );
 
         // Cuadro desplegable para elegir la región
-        $value = isset( $values['gifts_depending_precio_minimo'] ) ? esc_attr( $values['gifts_depending_precio_minimo'][0] ) : '';
+        $value = isset( $values['gifts_depending_minimum_price'] ) ? esc_attr( $values['gifts_depending_minimum_price'][0] ) : '';
         $output = '<p class="form-field _regular_price_field ">';
-        $output .= '<label for="gifts_depending_precio_minimo" class="gift-label">Precio Mínimo (S/.)</label>';
-        $output .= '<input type="number" name="gifts_depending_precio_minimo" id="gifts_depending_precio_minimo" class="short wc_input_price" value="'.$value.'" />';
+        $output .= '<label for="gifts_depending_minimum_price" class="gift-label">Minimum Price</label>';
+        $output .= '<input type="number" name="gifts_depending_minimum_price" id="gifts_depending_minimum_price" class="short wc_input_price" value="'.$value.'" />';
         $output .= '</p>';
 
         echo $output;
@@ -61,8 +62,8 @@ class GiftsDependingPurchase {
         if( !current_user_can( 'edit_post' ) ) return;
 
         // Guardamos los datos de región
-        if( isset( $_POST['gifts_depending_precio_minimo'] ) ) {
-            update_post_meta( $post_id, 'gifts_depending_precio_minimo', esc_attr( $_POST['gifts_depending_precio_minimo'] ) );
+        if( isset( $_POST['gifts_depending_minimum_price'] ) ) {
+            update_post_meta( $post_id, 'gifts_depending_minimum_price', esc_attr( $_POST['gifts_depending_minimum_price'] ) );
         }
 
     }
@@ -71,8 +72,8 @@ class GiftsDependingPurchase {
     function gifts_depending_columns_head($columns) {
         $new = array();
         foreach($columns as $key => $title) {
-            if ($key=='title') $new['foto'] = '<span class="dashicons dashicons-format-image"></span>';
-            if ($key=='author') $new['precio_minimo'] = 'Precio Mínimo';
+            if ($key=='title') $new['photo'] = '<span class="dashicons dashicons-format-image"></span>';
+            if ($key=='author') $new['minimum_price'] = 'Minimum Price';
             $new[$key] = $title;
         }
         return $new;
@@ -80,11 +81,11 @@ class GiftsDependingPurchase {
 
     // SHOW THE FEATURED IMAGE
     function gifts_depending_columns_content($column_name, $post_ID) {
-        if ($column_name == 'foto') {
+        if ($column_name == 'photo') {
             echo "<img src='".get_the_post_thumbnail_url($post_ID, 'thumbnail')."' style='width:100%; height:auto;' />";
         }
-        if ($column_name == 'precio_minimo') {
-            $precio = get_post_meta($post_ID, 'gifts_depending_precio_minimo', true);
+        if ($column_name == 'minimum_price') {
+            $precio = get_post_meta($post_ID, 'gifts_depending_minimum_price', true);
             if ($precio > 0) {
                 echo intval($precio);
             } else {
@@ -93,14 +94,15 @@ class GiftsDependingPurchase {
         }
     }
 
-    function my_sortable_gift_column( $columns ) {
-        $columns['precio_minimo'] = 'precio_minimo';
-
-        //To make a column 'un-sortable' remove it from the array
-        //unset($columns['date']);
-
+    function gifts_depending_sortable_column( $columns ) {
+        $columns['minimum_price'] = 'minimum_price';
         return $columns;
     }
+
+    function gifts_depending_admin_assets() {
+        wp_enqueue_style( 'gifts_depending_style', plugin_dir_url( __FILE__ ) . 'assets/css/gifts_depending.css', false );
+    }
+
 
 }
 ?>
